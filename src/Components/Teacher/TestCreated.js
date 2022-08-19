e28e3118c87842d828b25caefa7af194352635a1
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2022-08-19 16:00:22 +0530

    Added

--- src/Components/Teacher/TestCreated.js
+++ src/Components/Teacher/TestCreated.js
@@ -1,50 +1,63 @@
 import { Card, Col, Button } from "react-bootstrap";
 
 function TestCreated(props) {
-	console.log(props)
-	return (
-		<Col md={4}>
-			<Card
-				className="text-center"
-				style={{
-					margin: "1%",
-				}}
-			>
-				<Card.Header>Featured</Card.Header>
-				<Card.Body>
-					<Card.Title>{props.name}</Card.Title>
-					{/* <Card.Text
+  console.log(props);
+  const handleRemoveItem = (e) => {
+    console.log(e.target.id);
+  };
+  return (
+    <Col md={4}>
+      <Card
+        className="text-center"
+        style={{
+          margin: "1%",
+        }}
+      >
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Title>{props.name}</Card.Title>
+          {/* <Card.Text
 						style={{
 							textAlign: "justify",
 						}}
 					>
 						{props.description}
 					</Card.Text> */}
-					<br />
-					<Button
-						variant="outline-primary"
-						style={{
-							borderRadius: "20px",
-						}}
-						onClick={()=>{
-							props.setSubject(props.subject)
-							props.setInstructions(props.description)
-							props.handleShow()
-							props.setTestDetails({
-								id : props.test.unique_id,
-								q_id : props.test.questions[0].id
-							})
-							// window.location = `/question/${props.test.unique_id}/${props.test.questions[0].id}/edit`
-						}}
-					>
-						View Details
-					</Button>
-				</Card.Body>
-				<Card.Footer className="text-muted">{props.time}</Card.Footer>
-			</Card>
-		</Col>
-	);
+          <br />
+          <Button
+            variant="outline-primary"
+            style={{
+              borderRadius: "20px",
+              margin: "1%",
+            }}
+            onClick={() => {
+              props.setSubject(props.subject);
+              props.setInstructions(props.description);
+              props.handleShow();
+              props.setTestDetails({
+                id: props.test.unique_id,
+                q_id: props.test.questions[0].id,
+              });
+              // window.location = `/question/${props.test.unique_id}/${props.test.questions[0].id}/edit`
+            }}
+          >
+            View Details
+          </Button>
+          <Button
+            variant="outline-primary"
+            style={{
+              borderRadius: "20px",
+              margin: "1%",
+            }}
+            onClick={handleRemoveItem}
+          >
+            Delete Test
+          </Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">{props.time}</Card.Footer>
+      </Card>
+    </Col>
+  );
 }
 
-export default TestCreated
-
+export default TestCreated;
--- src/Components/Teacher/UploadQuestion.js
+++ src/Components/Teacher/UploadQuestion.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Alert, Button, Col, Row } from "react-bootstrap";
+import { Alert, Button, Col, Row, Card } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import axiosInstance from "../../axiosInstance";
@@ -10,84 +10,96 @@ import Loader from "./Loader";
 let access = getCookie("access_token");
 
 const headers = {
-    Authorization: `Bearer ${access}`,
-    "Content-Type": "application/json",
-  };
-
+  Authorization: `Bearer ${access}`,
+  "Content-Type": "application/json",
+};
 
 export default function UploadQuestion() {
-	const { id } = useParams();
-    // const [file, setFile] = useState(null)
-	const test_name = useSelector((state) => state.tests.test.name);
-    const [ error, setError] = useState(null)
-    const [loader , setLoader] = useState(false)
-    const file_ref = useRef()
-    
-    function upload_file(file){
-        let form_data = new FormData();
-        form_data.append("unique_id",id);
-        form_data.append("file",file);
-        setLoader(true)
-        axiosInstance.post('/api/test-upload/',form_data,{
-            headers : headers
-        })
-        .then((res=>{
-            if(res.status === 201){
-                window.location = `/question/${id}`;
-                file_ref.current.value = ""
-                setLoader(false)
-            }
-        }))
-        .catch((e)=>{
-            setError(e.response.data)
-            file_ref.current.value = ""
-            setTimeout(()=>{
-                setError(null)
-                window.location = '/teacher'
-            },1000)
-        })
-    }
+  const { id } = useParams();
+  // const [file, setFile] = useState(null)
+  const test_name = useSelector((state) => state.tests.test.name);
+  const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const file_ref = useRef();
 
-	return (
-		<div style={{
-            height: "100vh",
-            width: "100vw",
-            background: "white",
-        }}>
-            {loader && <Loader/>}
-			<h1 style={{textAlign:"center",position:"relative",top:"2em"}}>Upload File or Enter Manually</h1>
-            {error && <h5 style={{
-                textAlign : "center",
-                color : "red"
-            }}>{error}</h5>}
-			<div
-				style={{
-					height: "60vh",
-					width: "100vw",
-					background: "white",
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-			>
-				<Row>
-					<Col>
-						<input type={"file"} ref={file_ref} onChange={e=>{
-                            upload_file(e.target.files[0])
-                        }} />
-					</Col>
-					<Col>
-						<Button
-							onClick={() => {
-								window.location = `/question/${id}`;
-							}}
-						>
-							Enter Question Manually
-						</Button>
-					</Col>
-				</Row>
-                <br/>
-			</div>
-		</div>
-	);
+  function upload_file(file) {
+    let form_data = new FormData();
+    form_data.append("unique_id", id);
+    form_data.append("file", file);
+    setLoader(true);
+    axiosInstance
+      .post("/api/test-upload/", form_data, {
+        headers: headers,
+      })
+      .then((res) => {
+        if (res.status === 201) {
+          window.location = `/question/${id}`;
+          file_ref.current.value = "";
+          setLoader(false);
+        }
+      })
+      .catch((e) => {
+        setError(e.response.data);
+        file_ref.current.value = "";
+        setTimeout(() => {
+          setError(null);
+          window.location = "/teacher";
+        }, 1000);
+      });
+  }
+
+  return (
+    <div
+      style={{
+        padding: "0% 10%",
+        fontSize: "90%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        style={{
+          boxShadow: "0 8px 6px -6px black",
+          border: "none",
+          padding: "2%",
+          height: "50rem",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {loader && <Loader />}
+        <h3 style={{}}>Upload File or Enter Manually</h3>
+        {error && (
+          <h5
+            style={{
+              textAlign: "center",
+              color: "red",
+            }}
+          >
+            {error}
+          </h5>
+        )}
+        <br />
+        <br />
+        <center>
+        <input
+          type={"file"}
+          ref={file_ref}
+          onChange={(e) => {
+            upload_file(e.target.files[0]);
+          }}
+        />
+        </center>
+        <Button
+          variant="outline-primary"
+          style={{ margin: "1%", borderRadius: "20px", width: "30%" }}
+          onClick={() => {
+            window.location = `/question/${id}`;
+          }}
+        >
+          Enter Question Manually
+        </Button>
+      </Card>
+    </div>
+  );
 }
